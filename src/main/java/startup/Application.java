583da1e88b4d5dc7a82ab08292071646be5d5fbc
Mathieu POUSSE <mathieu.pousse@zenika.com>
2016-03-20 23:24:26 +0100

    make it fine!
    Signed-off-by: Mathieu POUSSE <mathieu.pousse@zenika.com>

--- src/main/java/startup/Application.java
+++ src/main/java/startup/Application.java
@@ -2,6 +2,8 @@ package startup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,8 +12,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication
 @EnableJpaRepositories
-@PropertySource({"git.properties", "marketing.properties"})
-public class Application {
+@PropertySource({"classpath:git.properties", "classpath:marketing.properties"})
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
